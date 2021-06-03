96d273c8b36ff2f736ea6c4671ab5874152454d1
seungchae1 <80873640+seungchae1@users.noreply.github.com>
2021-06-03 13:48:11 +0900

    [문법설명] switch로 바꾸기 (break로 의도한 코드만 실행)

--- 07-01 switch.c
+++ 07-01 switch.c
@@ -2,15 +2,18 @@
 
 void main(void)
 {
-	int a = 1;
+	int a = 3;
 
 	switch (a)
 	{
 	case 1:
 		printf("한화\n");
+		break;
 	case 2:
 		printf("감독님\n");
+		break;
 	default:
 		printf("예에\n");
+		break;
 	}
 }
