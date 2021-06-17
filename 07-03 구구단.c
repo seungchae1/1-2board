09cff53c88a7f605101501a85aadd7171ecaabfc
seungchae1 <80873640+seungchae1@users.noreply.github.com>
2021-06-17 15:05:48 +0900

    [문법설명] 구구단을 while문으로 변환

--- 07-03 구구단.c
+++ 07-03 구구단.c
@@ -3,10 +3,22 @@
 void main(void)
 {
 
-	for (int dan = 2; dan <= 9; dan++) {
+	/*for (int dan = 2; dan <= 9; dan++) {
 		for (int i = 1; i <= 9; i++) {
 			printf("%d X %d =%d\n", dan, i, dan * i);
 		}
+		printf("\n");*/
+	int dan=2;
+	int i=1;
+	while(dan<=9)
+	{
+		while (i <= 9) 
+		{
+			printf("%d X %d =%d\n", dan, i, dan * i);
+			i++;
+		}
+		i = 1;
+		dan++;
 		printf("\n");
 	}
 }
