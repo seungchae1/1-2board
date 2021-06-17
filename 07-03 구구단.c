40d2ad5653d7c30324851637af2465c93dc71bdc
seungchae1 <80873640+seungchae1@users.noreply.github.com>
2021-06-17 14:06:22 +0900

    [문법연습] 3,4단 무식하게 추가

--- 07-03 구구단.c
+++ 07-03 구구단.c
@@ -6,4 +6,12 @@ void main(void)
 	for (int i = 1; i <= 9; i++) {
 		printf("%d X %d =%d\n", dan,i, dan * i);
 	}
+	dan = 3;
+	for (int i = 1; i <= 9; i++) {
+		printf("%d X %d =%d\n", dan, i, dan * i);
+	}
+	dan = 4;
+	for (int i = 1; i <= 9; i++) {
+		printf("%d X %d =%d\n", dan, i, dan * i);
+	}
 }
