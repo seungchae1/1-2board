589184afa7685462ad81f3a49d45ef9bfb266248
seungchae1 <80873640+seungchae1@users.noreply.github.com>
2021-06-17 13:59:44 +0900

    [문법연습] while을 for문으로

--- 07-03 구구단.c
+++ 07-03 구구단.c
@@ -3,12 +3,7 @@
 void main(void)
 {
 	int dan = 2;
-	/*for (int i = 1; i < 10; i++) {
-		printf("%d X %d = %d\n", dan, i, dan * i);
-	} */
-	int i = 1;
-	while (i < 10) {
-		printf("%d X %d = %d\n", dan, i, dan * i);
-		i++;
+	for (int i = 1; i <= 9; i++) {
+		printf("%d X %d =%d\n", dan,i, dan * i);
 	}
 }
