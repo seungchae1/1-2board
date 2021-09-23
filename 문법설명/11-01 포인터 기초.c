ba59f9304013cf0dacf3fbf8be7f76c21afa5561
박승채 <80873640+seungchae1@users.noreply.github.com>
2021-09-23 13:46:21 +0900

    포인터 기초

---
+++ 문법설명/11-01 포인터 기초.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main(void)
+{
+	int num = 3;
+	//ptr이라는 포인터 변수를 만들어서 num을 가리킴
+	int* ptr = &num;
+	printf("num 안에 들어있는 값 : %d\n", num);
+	printf("num의 메모리 주소값 : %p\n", &num);
+	printf("ptr 안에 들어있는 값 : %p\n", ptr);
+	
+}
