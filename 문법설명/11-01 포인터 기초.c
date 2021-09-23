de12ea0e750ce5b4b694168966bb2a50da864ade
박승채 <80873640+seungchae1@users.noreply.github.com>
2021-09-23 13:49:46 +0900

    주소값은 %p로 표현해야 한다

--- 문법설명/11-01 포인터 기초.c
+++ 문법설명/11-01 포인터 기초.c
@@ -6,7 +6,7 @@ void main(void)
 	//ptr이라는 포인터 변수를 만들어서 num을 가리킴
 	int* ptr = &num;
 	printf("num 안에 들어있는 값 : %d\n", num);
-	printf("num의 메모리 주소값 : %d\n", &num);
-	printf("ptr 안에 들어있는 값 : %d\n", ptr);
+	printf("num의 메모리 주소값 : %p\n", &num);
+	printf("ptr 안에 들어있는 값 : %p\n", ptr);
 	
 }
