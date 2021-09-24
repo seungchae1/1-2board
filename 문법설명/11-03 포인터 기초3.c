247e4feadec7ee2951831266b690d359c5109b24
박승채 <80873640+seungchae1@users.noreply.github.com>
2021-09-24 12:07:48 +0900

    가리키는 대상을 바꿈

---
+++ 문법설명/11-03 포인터 기초3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main(void)
+{
+	int num1 = 3, num2 = 30;
+	printf("num1안에 들어있는 값 : %d\n", num1);
+	printf("num2안에 들어있는 값 : %d\n", num2);
+
+	int* ptr = &num1;
+	*ptr = 4;
+	ptr = &num2;
+	printf("num1안에 들어있는 값 : %d\n", num1);
+	printf("num2안에 들어있는 값 : %d\n", num2);
+}
