4a2111d31d941cbf8fb202d4ead12261c15f8c4d
박승채 <80873640+seungchae1@users.noreply.github.com>
2021-10-14 14:45:51 +0900

    두 변수의 값을 함수로 바꾸기

---
+++ 문법설명/11-06 Swap.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void Swap(int *num1,int *num2);
+
+void main(void)
+{
+	int a = 10, b = 20;
+	printf("바꾸기 전 a:%d b:%d \n",a,b);
+	Swap(&a, &b);
+	printf("바꾸기 후 a:%d b:%d \n",a,b);
+}
+
+void Swap(int *num1, int *num2)
+{
+	int* temp = *num1;
+	*num1 = *num2;
+	*num2 = temp;
+}
