63698df486156ef142a2b74de31ad736fbc0d35a
박승채 <80873640+seungchae1@users.noreply.github.com>
2021-11-11 14:56:41 +0900

    배열을 매개변수에 넘기려면 포인터로 넘겨야 한다.
    메모리 용량을 아끼기 위해

---
+++ 문법설명/12-05 배열과 함수.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void print(int ptr[]);
+void erase(int ptr[]);
+
+void main(void) {
+	int arr[4] = { 10,20,30,40 };
+	erase(arr);
+	print(arr);
+}
+
+//배열의 모든 요소(원소)를 출력
+void print(int ptr[])
+{
+	//i<4는 배열의 크기가 4인것만 가능함(구조적 결함)
+	for (int i = 0; i < 4; i++) {
+		//printf("%d ", *(ptr+i));
+		printf("%d ", ptr[i]);
+	}
+	printf("\n");
+}
+
+void erase(int ptr[])
+{
+	for (int i = 0; i < 4; i++) {
+		ptr[i] = 0;
+	}
+}
